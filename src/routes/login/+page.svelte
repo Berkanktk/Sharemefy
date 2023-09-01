0530dfc7615e5b123a53c83783ddcf674e9217bb
Berkanktk <berkank@live.dk>
2023-09-01 15:12:54 +0200

    Implemented Cookie-based auth

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,22 +1,38 @@
 <script lang="ts">
   import { auth, user } from "$lib/firebase";
+  import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
-    import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+  async function signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    const credential = await signInWithPopup(auth, provider);
+    const idToken = await credential.user.getIdToken();
 
+    const res = await fetch("/api/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // 'CSRF-Token': csrfToken  // HANDLED by sveltekit automatically
+      },
+      body: JSON.stringify({ idToken }),
+    });
+  }
 
-    async function signInWithGoogle() {
-        const provider = new GoogleAuthProvider();
-        const user = await signInWithPopup(auth, provider);
-        console.log(user)
-    }
-
+  async function signOutSSR() {
+    const res = await fetch("/api/signin", { method: "DELETE" });
+    await signOut(auth);
+  }
+  
 </script>
 
 {#if $user}
   <h2 class="card-title">Welcome, {$user.displayName}</h2>
   <p class="text-center text-success">You are logged in</p>
-  <button class="btn btn-warning" on:click={() => signOut(auth)}>Sign out</button>
+  <button class="btn btn-warning" on:click={signOutSSR}
+    >Sign out</button
+  >
 {:else}
   <h2>Create an account by logging into your Google account.</h2>
-  <button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
+  <button class="btn btn-primary" on:click={signInWithGoogle}
+    >Sign in with Google</button
+  >
 {/if}
